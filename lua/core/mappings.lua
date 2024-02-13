0b1931cea4a79b98868f167ff0b1711b0ceb175d
ToshaRotten <kozlovad@mer.ci.nsu.ru>
2024-02-13 11:59:51 +0700

    golang and python3 mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,6 +8,14 @@ vim.keymap.set('n', '<leader>q', ':q<CR>')
 vim.keymap.set('n', '<leader>s', ':w<CR>')
 vim.keymap.set('n', '<leader>`', ':Lazy<CR>')
 
+
+-- Golang mapping
+vim.keymap.set('n', '<leader>r', ':GoRun main.go<CR>')
+
+-- Python mapping
+vim.keymap.set('n', '<leader>p', ':python3 main.go<CR>')
+
+
 -- Todo
 
 -- Fast exit to normal mode 
