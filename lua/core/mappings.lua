66cf815e739532cc2bd0812a6dcd2f8191e75875
ToshaRotten <kozlovad@mer.ci.nsu.ru>
2024-02-15 18:16:43 +0700

    Add code editor mapping and utils mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,6 +8,9 @@ vim.keymap.set('n', '<leader>q', ':q<CR>')
 vim.keymap.set('n', '<leader>s', ':w<CR>')
 vim.keymap.set('n', '<leader>`', ':Lazy<CR>')
 
+-- Code editor mapping
+vim.keymap.set('i', '<C-l>', '<DEL>')
+vim.keymap.set('i', '<leader>h', '<DEL>')
 
 -- Golang mapping
 vim.keymap.set('n', '<leader>r', ':GoRun main.go<CR>')
@@ -15,6 +18,8 @@ vim.keymap.set('n', '<leader>r', ':GoRun main.go<CR>')
 -- Python mapping
 vim.keymap.set('n', '<leader>p', ':python3 main.go<CR>')
 
+-- Utils mapping
+vim.keymap.set('n', '<leader>m, ':make')
 
 -- Todo
 
